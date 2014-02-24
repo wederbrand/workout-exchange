0924f834b44f8c4a8622bcea57937691b60d5990
Andreas Wederbrand <andreas@wederbrand.se>
2014-02-24 10:27:36 +0100

    Fixes #7
    - login through garmins new SSO infrastructure

--- garmin_connect/download_all.rb
+++ garmin_connect/download_all.rb
@@ -13,17 +13,57 @@ end
 
 agent = Mechanize.new { |agent|
   agent.user_agent_alias= 'Mac Safari'
+  agent.redirect_ok = true
+  agent.follow_meta_refresh = true
   agent.pluggable_parser['text/xml'] = Mechanize::Page
   agent.pluggable_parser['application/vnd.garmin.gpx+xml'] = Mechanize::Download
 }
 
 # first log in
-page = agent.get('http://connect.garmin.com/signin')
-form = page.form_with(:name => 'login')
+page = agent.get('http://connect.garmin.com/en-US/signin')
 
-form.field_with(:name => 'login:loginUsernameField').value = user
-form.field_with(:name => 'login:password').value = password
-form.click_button
+# find and load script
+script_src = page.search("script [src]").select{ |script| script['src'].include? "sso.garmin.com"  }.first['src']
+script = agent.get(script_src)
+
+# get the gauth host name
+hostname_page = agent.get('http://connect.garmin.com/gauth/hostname')
+hostname = JSON::parse(hostname_page.body)['host']
+
+# Get the iframe with the login form
+login_page = agent.get('https://sso.garmin.com/sso/login', {
+  'service' => 'http://connect.garmin.com/post-auth/login',
+  'webhost' => hostname,
+  'source' => 'http://connect.garmin.com/en-US/signin',
+  'redirectAfterAccountLoginUrl' => 'http://connect.garmin.com/post-auth/login',
+  'redirectAfterAccountCreationUrl' => 'http://connect.garmin.com/post-auth/login',
+  'gauthHost' => 'https://sso.garmin.com/sso',
+  'locale' => 'en',
+  'id' => 'gauth-widget',
+  'cssUrl' => 'https://static.garmincdn.com/com.garmin.connect/ui/src-css/gauth-custom.css',
+  'clientId' => 'GarminConnect',
+  'rememberMeShown' => 'true',
+  'rememberMeChecked' => 'false',
+  'createAccountShown' => 'true',
+  'openCreateAccount' => 'false',
+  'usernameShown' => 'true',
+  'displayNameShown' => 'false',
+  'consumeServiceTicket' => 'false',
+  'initialFocus' => 'true',
+  'embedWidget' => 'false'
+})
+
+# fill out and submit form
+form = login_page.form_with(:id => 'login-form')
+form.field_with(:id => 'username').value = user
+form.field_with(:id => 'password').value = password
+form_result = form.click_button
+
+# find the url that gets us beck to connect.garmin.com
+login_url = form_result.body.scan(/response_url\s*=\s*'(.*)';/).first.first
+
+# and finally login to connect.garmin.com
+page = agent.get(login_url)
 
 # fetch the last 20 workouts
 page = agent.get('http://connect.garmin.com/proxy/activity-search-service-1.0/axm/activities?')
