328cd7a6689f8655778b134e75cf196a5f5e2741
Andreas Wederbrand <andreas@wederbrand.se>
2014-06-11 21:25:43 +0200

    Change name of file-link

--- strava/upload_one.rb
+++ strava/upload_one.rb
@@ -11,22 +11,21 @@ a = Mechanize.new { |agent|
   agent.follow_meta_refresh = true
 }
 
-a.get('http://www.strava.com/') do |home_page|
-  signin_page = a.click(home_page.link_with(:text => /Log In/))
+home_page = a.get('http://www.strava.com/')
+signin_page = a.click(home_page.link_with(:text => /Log In/))
 
-  my_page = signin_page.form_with(:id => 'login_form') do |form|
-    form.email  = ARGV[0]
-    form.password = ARGV[1]
-  end.submit
+form = signin_page.form_with(:id => 'login_form');
+form.email  = 'andreas@wederbrand.se' #ARGV[0]
+form.password = 'ash3ml1gt' #ARGV[1]
+my_page = form.submit
 
-  # Click the upload link
-  upload_page = a.click(my_page.link_with(:text => /Upload/))
+# Click the upload link
+upload_page = a.click(my_page.link_with(:text => /Upload/))
 
-  # Upload from File
-  upload_page = a.click(upload_page.link_with(:text => /From File/))
+# Upload from File
+upload_page = a.click(upload_page.link_with(:text => /File/))
 
-  # Upload the files
-  upload_page.form_with(:method => 'POST') do |upload_form|
-    upload_form.file_uploads.first.file_name = ARGV[2]
-  end.submit
-end
+# Upload the files
+upload_page.form_with(:method => 'POST') do |upload_form|
+  upload_form.file_uploads.first.file_name = ARGV[2]
+end.submit
